5ca2ece1773013fc436e79b17fcafaae38092c88
the0hdDev <theowimber@gmail.com>
2025-07-05 16:55:33 +0200

    EnterCData work continued

--- src/frontend/js/enterCData.js
+++ src/frontend/js/enterCData.js
@@ -1,18 +1,26 @@
 const input_ip = document.getElementById('input_ip');
 
 function submitButtonClicked() {
-     const ip_strng = document.getElementById('input_ip').value; // KEINE Klammern
-     if (ip_strng != "localhost" && ip_strng != "") {
-         const ip = parseInt(ip_strng);
-         alert('normal')
-     } else if (ip_strng == "") {
-        alert('Enter a Valid Ip')
-     }
-     alert(ip);
-     alert(ip_strng);
+    const ip_strng = input_ip.value; // trim entfernt Leerzeichen
+    let ip;
 
+    if (ip_strng === "") {
+        alert('Enter a valid IP');
+        return;
+    }
 
-     if(ip == 'NaN' ) {
-        alert('Please Input a Valid Ip Address!')
-     } 
+    if (ip_strng === "localhost") {
+        alert('localhost detected');
+    } else {
+        ip = parseInt(ip_strng);
+        if (isNaN(ip)) {
+            alert('Invalid number');
+            return;
+        } else {
+            alert('Normal number input');
+        }
+    }
+
+    alert('Parsed ip (number): ' + ip);
+    alert('Original input: ' + ip_strng);
 }
