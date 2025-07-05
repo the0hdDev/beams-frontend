16e1be4e8d7769f87233bc2d77b694236d56635e
Theo <theowimber@gmail.com>
2025-07-05 13:50:26 +0200

    enterCData Frontend Completed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 500,
+    minimizable: false,
+    resizable: false,
     icon: __dirname + '/frontend/assets/favicon.png'
   })
 
-  win.loadFile('./src/frontend/index.html')
+  win.loadFile('./src/frontend/enterCData.html')
 }
 
 app.whenReady().then(() => {
